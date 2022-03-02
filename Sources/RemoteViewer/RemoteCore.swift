9ae82cb6406b90345583f66e67669e2a701f64b8
Douglas Adams <douglas.adams@me.com>
2022-03-02 11:15:50 -0500

    v2.4.1

--- Sources/RemoteViewer/RemoteCore.swift
+++ Sources/RemoteViewer/RemoteCore.swift
@@ -1,26 +1,172 @@
 //
-//  RemoteCore.swift
-//  Components6000/RemoteViewer
+//  RelayCore.swift
+//  Components6000/DinRelay
 //
-//  Created by Douglas Adams on 1/19/22.
+//  Created by Douglas Adams on 2/26/22.
 //
 
 import Foundation
 import ComposableArchitecture
+import Combine
+
+// ----------------------------------------------------------------------------
+// MARK: - Structs and Enums
+
+struct RelaySubscriptionId: Hashable {}
+
+public struct Relay: Codable, Equatable {
+  
+  public init(
+    critical: Bool = false,
+    transientState: Bool = false,
+    physicalState: Bool = false,
+    state: Bool = false,
+    name: String,
+    cycleDelay: Int? = nil,
+    locked: Bool = false
+  ) {
+    self.critical = critical
+    self.transientState = transientState
+    self.physicalState = physicalState
+    self.state = state
+    self.name = name
+    self.cycleDelay = cycleDelay
+    self.locked = locked
+  }
+  public var critical: Bool
+  public var transientState: Bool
+  public var physicalState: Bool
+  public var state: Bool
+  public var name: String
+  public var cycleDelay: Int?
+  public var locked: Bool
+}
+
+// ----------------------------------------------------------------------------
+// MARK: - State, Actions & Environment
 
 public struct RemoteState: Equatable {
-  public init() {}
+  
+  public init(_ heading: String = "Relay Status") {
+    self.heading = heading
+  }
+  public var heading: String
+  public var relays = initialRelays
+  public var forceUpdate = false
 }
 
 public enum RemoteAction: Equatable {
-  case cancel
+  case allOff
+  case onAppear
+  case refresh
+  case relayLoadFailed
+  case relaysReceived([Relay])
+  case start
+  case stop
+  case toggleState(Int)
+  case toggleLocked(Int)
 }
 
 public struct RemoteEnvironment {
   public init() {}
 }
 
+// ----------------------------------------------------------------------------
+// MARK: - Reducer
+
 public let remoteReducer = Reducer<RemoteState, RemoteAction, RemoteEnvironment>
   { state, action, environment in
+    
+    switch action {
+      // ----------------------------------------------------------------------------
+      // MARK: - Initialization
+      
+    case .onAppear:
+      return getRelays( relayRequest("admin", "ruwn1viwn_RUF_zolt") )
+
+      // ----------------------------------------------------------------------------
+      // MARK: - RelayView UI actions
+      
+    case .allOff:
+      print("-----> All Off")
+      for (i, relay) in state.relays.enumerated() {
+        state.relays[i].state = false
+      }
       return .none
+      
+    case .refresh:
+      return getRelays( relayRequest("admin", "ruwn1viwn_RUF_zolt") )
+    
+    case .toggleLocked(let index):
+      state.relays[index].locked.toggle()
+      return .none
+
+    case .toggleState(let index):
+      state.relays[index].state.toggle()
+      return .none
+    
+    case .relaysReceived(let relays):
+      state.relays = relays
+      state.forceUpdate.toggle()
+      return .none
+    
+    case .relayLoadFailed:
+      return .none
+    
+    case .start:
+      return .none
+    
+    case .stop:
+      return .none
+    }
   }
+
+
+// ----------------------------------------------------------------------------
+// MARK: - Helper functions
+
+public var initialRelays = [
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized"),
+  Relay(name: "uninitialized")
+]
+
+extension URLRequest {
+  mutating func setBasicAuth(username: String, password: String) {
+    let encodedAuthInfo = String(format: "%@:%@", username, password)
+      .data(using: String.Encoding.utf8)!
+      .base64EncodedString()
+    addValue("Basic \(encodedAuthInfo)", forHTTPHeaderField: "Authorization")
+    addValue("x", forHTTPHeaderField: "X-CSRF")
+  }
+}
+
+func relayRequest(_ user: String, _ pwd: String) -> URLRequest {
+  var request = URLRequest(url: URL(string: "https://192.168.1.220/restapi/relay/outlets/")!)
+  request.setBasicAuth(username: user, password: pwd)
+  request.httpMethod = "GET"
+  return request
+}
+
+func getRelays(_ request: URLRequest) -> Effect<RemoteAction, Never> {
+  return URLSession.DataTaskPublisher(request: request, session: .shared)
+    .receive(on: DispatchQueue.main)
+    .catchToEffect()
+    .map { result in
+      switch result {
+      case .success(let output):
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return .relaysReceived(try! decoder.decode([Relay].self, from: output.data))
+        
+      case .failure:
+        return .relayLoadFailed
+      }
+    }
+    .eraseToEffect()
+}
--- Sources/RemoteViewer/RemoteView.swift
+++ Sources/RemoteViewer/RemoteView.swift
@@ -1,12 +1,15 @@
 //
 //  RemoteView.swift
-//  Components6000/RemoteViewer
+//  Components6000/DinRelay
 //
-//  Created by Douglas Adams on 2/17/22.
+//  Created by Douglas Adams on 2/26/22.
 //
 
-import SwiftUI
 import ComposableArchitecture
+import SwiftUI
+
+// ----------------------------------------------------------------------------
+// MARK: - View(s)
 
 public struct RemoteView: View {
   let store: Store<RemoteState, RemoteAction>
@@ -14,17 +17,84 @@ public struct RemoteView: View {
   public init(store: Store<RemoteState, RemoteAction>) {
     self.store = store
   }
-    public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  public var body: some View {
+    
+    WithViewStore(store) { viewStore in
+      VStack {
+        Text(viewStore.heading).font(.title).padding(.vertical)
+        HStack {
+          Text("Number").frame(width: 100, alignment: .leading)
+          Text("Name").frame(width: 250, alignment: .leading)
+          Text("State").frame(width: 100, alignment: .center)
+          Text("Locked").frame(width: 100, alignment: .center)
+          Text("Cycle Delay").frame(width: 150, alignment: .center)
+        }.font(.title2)
+        
+        Divider().background(Color(.red))
+        ForEach(viewStore.relays.indices) { index in
+          HStack {
+            VStack {
+              Text("\(index + 1)")
+            }.frame(width: 100, alignment: .leading)
+
+            VStack {
+              Text(viewStore.relays[index].name == "" ? "-- none --" : viewStore.relays[index].name)
+            }.frame(width: 250, alignment: .leading)
+
+            VStack {
+              Toggle("", isOn: viewStore.binding(get: \.relays[index].state, send: .toggleState(index)))
+            }.frame(width: 100, alignment: .center)
+            
+            VStack {
+              Toggle("", isOn: viewStore.binding(get: \.relays[index].locked, send: .toggleLocked(index)))
+            }.frame(width: 100, alignment: .center)
+          
+            VStack {
+              Text(viewStore.relays[index].cycleDelay == nil ? "-- none --" : "\(viewStore.relays[index].cycleDelay!)")
+            }.frame(width: 150, alignment: .center)
+          }
+          .font(.title2)
+        }
+        Spacer()
+        Divider().background(Color(.red))
+        HStack(spacing: 60) {
+          Button("Refresh") { viewStore.send(.refresh) }
+          Button("All Off") { viewStore.send(.allOff) }
+          Spacer()
+          Button("Start") { viewStore.send(.start) }
+          Button("Stop") { viewStore.send(.stop) }
+        }
+        .onAppear() { viewStore.send(.onAppear) }
+      }
+      .padding()
     }
+  }
 }
 
+// ----------------------------------------------------------------------------
+// MARK: - Preview(s)
+
 struct RemoteView_Previews: PreviewProvider {
+  
   static var previews: some View {
-    RemoteView(store: Store(
-      initialState: RemoteState(),
-      reducer: remoteReducer,
-      environment: RemoteEnvironment())
+    RemoteView(
+      store: Store(
+        initialState: RemoteState("Test Relays"),
+        reducer: remoteReducer,
+        environment: RemoteEnvironment()
+      )
     )
   }
 }
+
+public var testRelays: [Relay] = [
+  Relay(name: "Main power"),
+  Relay(name: "---"),
+  Relay(name: "---"),
+  Relay(name: "---"),
+  Relay(name: "Astron power"),
+  Relay(name: "Rotator power"),
+  Relay(name: "Linear power"),
+  Relay(name: "Remote power on")
+]
